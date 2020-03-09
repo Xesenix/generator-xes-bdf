22bbb72863be5b4dc18bc247a62e281e5747fd05
Paweł Kapalla <pkapalla@xesenix.pl>
2020-03-09 02:22:03 +0100

    test: setup jest lookup paths

--- __tests__/app.js
+++ __tests__/app.js
@@ -7,8 +7,23 @@ describe('generator-xes-bdf:app', () => {
 	beforeAll(() => {
 		return helpers
 			.run(path.join(__dirname, '../generators/app'))
+			.withOptions({
+				skipCache: false,
+				skipInstall: true,
+			})
 			.withPrompts({
-				someAnswer: true,
+				name: 'Test',
+				projectDescription: 'Description',
+				author: 'Tester',
+				npmInstall: 'yes',
+				usePhaser: 'yes',
+				usePixi: 'yes',
+				useReact: 'yes',
+				indentStyle: 'tab',
+				indentSize: '2',
+				quote: 'single',
+				initGit: 'yes',
+				initGitIgnore: 'yes',
 			});
 	});
 
