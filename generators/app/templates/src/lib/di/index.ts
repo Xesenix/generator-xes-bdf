541d58550735c58c5bfb869e837c0afa047c0694
Paweł Kapalla <pkapalla@xesenix.pl>
2020-05-25 04:29:36 +0200

    fix(di): add missing export

--- generators/app/templates/src/lib/di/index.ts
+++ generators/app/templates/src/lib/di/index.ts
@@ -2,4 +2,5 @@ export * from './connect-to-di';
 export * from './connect-to-injector';
 export * from './context';
 export * from './decorators';
+export * from './get-dependencies';
 export * from './use-injector';
